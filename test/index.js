be86af6fef39635629380f1ba7f43c072bdbebd5
Diana Thayer <garbados@gmail.com>
2015-08-05 15:34:22 -0700

    begin training

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var path = require('path');
 
 var lib;
@@ -11,13 +12,17 @@ var FIXTURES_DIR = process.env.ABC_FIXTURES_DIR || path.join(__dirname, '..', 'f
 
 describe('lib', function () {
   before(function () {
-    this.quantquote = {};
-    this.quantquote.dir = path.join(FIXTURES_DIR, 'quantquote');
-
+    this.training_file = path.join(FIXTURES_DIR, 'quantquote_daily_sp500_83986', 'daily', 'table_goog.csv');
     this.capitalist = new lib.Capitalist();    
   });
 
-  it('should train on quantquote data', function () {
-    return this.capitalist.train_from_file(this.quantquote.dir);
+  it('should train on fixtures data', function () {
+    var self = this;
+    return this.capitalist.train(this.training_file)
+      .then(function () {
+        var test_data = self.capitalist.nets.goog.firstDatum;
+        var test_output = self.capitalist.nets.goog.neuralNetwork.run(test_data.input);
+        assert.equal(test_output, test_data.output);
+      });
   });
 });
